cbd4dbccaba58d5f8725892deb0524f94c5b9b20
amor.edlouise <amor.edlouise@gmail.com>
2020-07-12 02:54:12 +0800

    Updates

---
+++ precache-manifest.5f3974fc1d7db0231a344672bb49df4c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122cbc930bdd726bab2a221387638a37",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "24d70c397485e921b1b1",
+    "url": "/portfolio2/static/css/main.ccd5d064.chunk.css"
+  },
+  {
+    "revision": "ba1097dc8963be2e51a3",
+    "url": "/portfolio2/static/js/2.697515fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio2/static/js/2.697515fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24d70c397485e921b1b1",
+    "url": "/portfolio2/static/js/main.4bd94542.chunk.js"
+  },
+  {
+    "revision": "26c8f6b3aa9d3d66cbba",
+    "url": "/portfolio2/static/js/runtime-main.78eb13dd.js"
+  },
+  {
+    "revision": "6d7dc4dcb24ccb9d1af0c5152b0f7f10",
+    "url": "/portfolio2/static/media/capstone1.6d7dc4dc.jpg"
+  },
+  {
+    "revision": "8458dd459bbe7fbdf721cf49c96a7ffb",
+    "url": "/portfolio2/static/media/capstone2.8458dd45.jpg"
+  },
+  {
+    "revision": "ad1842230fd42e9acab565b193086b48",
+    "url": "/portfolio2/static/media/cueonlinestore.ad184223.jpg"
+  },
+  {
+    "revision": "f6fe618f3fa3e961f5859c6e30644c3c",
+    "url": "/portfolio2/static/media/todoapp.f6fe618f.jpg"
+  }
+]);
